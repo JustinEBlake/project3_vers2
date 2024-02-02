e7ed17105ad12f55a7c0a6f1f74c050769a6a96c
JustinEBlake <116922273+JustinEBlake@users.noreply.github.com>
2024-02-01 19:56:00 -0600

    fixed func

--- pages/3_📊Bar Chart.py
+++ pages/3_📊Bar Chart.py
@@ -3,9 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-def make_chart(file,x_axis, y_axis):
-    df = pd.DataFrame(file)
+def make_chart(file,col1, col2):
+    df = pd.read_csv(file)
 
+    x_axis = col1
+    y_axis = col2
     chart = st.bar_chart(df,x=x_axis,y=y_axis)
 
     return chart
@@ -20,4 +22,5 @@ x_axis = st.text_input("Input column for X Axis")
 y_axis = st.text_input("Input column for Y Axis")
 
 
-st.button("Click to Chart Uploaded Data", on_click=make_chart(file, x_axis, y_axis))
+if st.button("Click to Chart Uploaded Data"):
+    make_chart(file,x_axis, y_axis)
